edb5039af352cb983dd2d56cf6261adaa6dfa209
BenjaminDanielsen <bda048@uit.no>
2025-07-03 10:46:47 +0200

    Price history

---
+++ utils/price_history_fetcher.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+from datetime import datetime
+
+
+def fetch_price_history_coingecko(coin_id, start_time):
+    end_time = datetime.now()
+    start_timestamp = int(start_time.timestamp())
+    end_timestamp = int(end_time.timestamp())
+
+    url = f"https://api.coingecko.com/api/v3/coins/{coin_id}/market_chart/range"
+    params = {"vs_currency": "usd", "from": start_timestamp, "to": end_timestamp}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if response.status_code != 200:
+        raise Exception(f"Error {response.status_code}: {data}")
+
+    prices = data.get("prices", [])
+    if not prices:
+        raise Exception(
+            f"No price data found for {coin_id} from {start_time} to {end_time}."
+        )
+
+    df = pd.DataFrame(prices, columns=["timestamp", "price"])
+    df["timestamp"] = df["timestamp"] // 1000
+    df["datetime"] = pd.to_datetime(df["timestamp"], unit="s")
+    df.set_index("datetime", inplace=True)
+    df.drop(columns=["timestamp"], inplace=True)
+    return df
